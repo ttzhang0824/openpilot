826a2649b6e6abed85705e3f996a965ea1837810
Mathew Locoteta <m.locoteta@gmail.com>
2021-03-04 17:41:38 -0500

    Updated for V2 Board serialSteering

--- selfdrive/car/honda/interface.py
+++ selfdrive/car/honda/interface.py
@@ -220,7 +220,7 @@ class CarInterface(CarInterfaceBase):
       ret.wheelbase = 2.75
       ret.centerToFront = ret.wheelbase * 0.39
       ret.steerRatio = 13.23 
-      ret.lateralParams.torqueBP, ret.lateralParams.torqueV = [[0, 239], [0, 239]]  # TODO: determine if there is a dead zone at the top end
+      ret.lateralParams.torqueBP, ret.lateralParams.torqueV = [[0, 238], [0, 238]]  # TODO: determine if there is a dead zone at the top end
       ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.24], [0.08]]      
       tire_stiffness_factor = 0.8467
       ret.longitudinalTuning.kpBP = [0., 5., 35.]
