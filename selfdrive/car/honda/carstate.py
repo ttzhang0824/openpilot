ddf04bb43f74a4e9c52a270151526ac168be0d1d
Mathew Locoteta <m.locoteta@gmail.com>
2021-04-30 22:02:02 -0400

    removed 0xe4,100 checks

--- selfdrive/car/honda/carstate.py
+++ selfdrive/car/honda/carstate.py
@@ -433,7 +433,7 @@ class CarState(CarStateBase):
                   ("ICONS", "ACC_HUD", 0)]
 
     # all hondas except CRV, RDX and 2019 Odyssey@China use 0xe4 for steering
-    checks = [(0xe4, 100)]
+    #checks = [(0xe4, 100)]
     if CP.carFingerprint in [CAR.CRV, CAR.CRV_EU, CAR.ACURA_RDX, CAR.ODYSSEY_CHN]:
       checks = [(0x194, 100)]
     if CP.carFingerprint in (CAR.ACCORD_NIDEC_SS):
