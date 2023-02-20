6f3059828dca7e01d9433c24cf090a843ce83aca
coll0412 <coll0412@gmail.com>
2023-02-19 22:56:46 -0600

    correct indentation again in carstate.py

--- selfdrive/car/honda/carcontroller.py
+++ selfdrive/car/honda/carcontroller.py
@@ -159,29 +159,29 @@ class CarController:
     # steer torque is converted back to CAN reference (positive when steering right)
     apply_steer = int(interp(actuators.steer * P.STEER_MAX, P.STEER_LOOKUP_BP, P.STEER_LOOKUP_V))
     if (CS.CP.carFingerprint in SERIAL_STEERING):
-       apply_steer = apply_std_steer_torque_limits(apply_steer, self.apply_steer_last, CS.out.steeringTorque, LKAS_LIMITS, ss=True)
-       self.apply_steer_last = apply_steer
-       if apply_steer > 229 and False:
+        apply_steer = apply_std_steer_torque_limits(apply_steer, self.apply_steer_last, CS.out.steeringTorque, LKAS_LIMITS, ss=True)
+        self.apply_steer_last = apply_steer
+        if apply_steer > 229 and False:
            apply_steer_orig = apply_steer
            apply_steer = (apply_steer - 229) * 2 + apply_steer
-        if apply_steer > 240:
-            self.apply_steer_over_max_counter += 1
-            if self.apply_steer_over_max_counter > 3:
-                apply_steer = apply_steer_orig
-                self.apply_steer_over_max_counter = 0
+           if apply_steer > 240:
+               self.apply_steer_over_max_counter += 1
+               if self.apply_steer_over_max_counter > 3:
+                   apply_steer = apply_steer_orig
+                   self.apply_steer_over_max_counter = 0
             else:
                 self.apply_steer_over_max_counter = 0
         elif apply_steer < -229 and False:
-        apply_steer_orig = apply_steer
-        apply_steer = (apply_steer + 229) * 2 + apply_steer
-        if apply_steer < -240:
-            self.apply_steer_over_max_counter+= 1
+            apply_steer_orig = apply_steer
+            apply_steer = (apply_steer + 229) * 2 + apply_steer
+            if apply_steer < -240:
+                self.apply_steer_over_max_counter+= 1
             if self.apply_steer_over_max_counter > 3:
                 apply_steer = apply_steer_orig
                 self.apply_steer_over_max_counter = 0
             else:
                 self.apply_steer_over_max_counter = 0
-      else:
+    else:
         self.apply_steer_over_max_counter = 0
 
     self.apply_steer_last = apply_steer
