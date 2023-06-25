d1caf36e81fe53eea857d3d60999cdd4797df863
coll0412 <coll0412@gmail.com>
2023-06-24 19:07:31 -0500

    tuning adjustment and actuator delay

--- selfdrive/car/honda/interface.py
+++ selfdrive/car/honda/interface.py
@@ -292,7 +292,7 @@ class CarInterface(CarInterfaceBase):
         ret.steerRatio = 15.66  # as spec
         ret.lateralParams.torqueBP, ret.lateralParams.torqueV = [[0, 238], [0, 238]]  # TODO: determine if there is a dead zone at the top end
         tire_stiffness_factor = 0.444
-        ret.steerActuatorDelay = 0.1
+        ret.steerActuatorDelay = 0.3
         #ret.lateralTuning.pid.kf = 0.000035
         #ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.115], [0.052]]
         CarInterfaceBase.configure_torque_tune(candidate, ret.lateralTuning)
@@ -325,7 +325,7 @@ class CarInterface(CarInterfaceBase):
     ret.tireStiffnessFront, ret.tireStiffnessRear = scale_tire_stiffness(ret.mass, ret.wheelbase, ret.centerToFront,
                                                                          tire_stiffness_factor=tire_stiffness_factor)
 
-    ret.steerActuatorDelay = 0.1
+    #ret.steerActuatorDelay = 0.1
     ret.steerLimitTimer = 0.8
 
     return ret
